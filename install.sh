9ebdac204d05bd6621085da93acc3017e238a442
Alfons Hwu <alfons.cs04@g2.nctu.edu.tw>
2020-02-20 11:40:48 +0800

    tmux --> oh my tmux and minor fix

--- install.sh
+++ install.sh
@@ -27,9 +27,12 @@ if [ $is_sudoer -eq 1 ]; then
 	if [ "$(uname -s)" = "Linux" ]; then
     	echo $(uname -s)
 	    sudo apt-get update
-	    for i in zsh mosh vim tmux node install git wget python-dev python-pip python3-dev python3-pip nodejs xsel cmake; do
+	    for i in zsh mosh vim node install git wget python-dev python-pip python3-dev python3-pip nodejs xsel cmake; do
 	        sudo apt-get --yes --force-yes -f -m install $i
 	    done
+		
+		# install oh-my-tmux rather than original tmux
+		git clone https://github.com/gpakosz/.tmux.git
 
 	    # fix 'no such file: /usr/local/bin/node' issue
 	    sudo ln -s /usr/bin/nodejs /usr/local/bin/node
@@ -72,11 +75,11 @@ tmux_version_check=$(awk "BEGIN{ print ($tmux_version > $min_tmux_version) ? \"1
 if [ $tmux_version_check -eq 0 ];then # use mini tmux configure
     ln -sf $CLONE_PATH/tmux19.conf ~/.tmux.conf
 else
-    # install tmux-package-manager(TPM)
+    install tmux-package-manager(TPM)
     git_get https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
     ln -sf $CLONE_PATH/tmux.conf ~/.tmux.conf
 
-    # install packages
+    install packages
     ~/.tmux/plugins/tpm/scripts/install_plugins.sh
 fi
 
